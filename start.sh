5997bf76684d77c6402a7efb1aabb59f4d463821
ruslan2080707-hub <ruslan2080707@gmail.com>
2025-10-04 16:56:54 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,3 +7,5 @@ echo "Starting application on port $PORT"
 
 # Запускаем gunicorn с правильным портом
 exec gunicorn --bind 0.0.0.0:$PORT --workers 2 --timeout 120 src.main:app
+python run_bot.py &
+gunicorn src.main:app
